e7612255374c9d53659dc1e65c019dedcf6a3851
vanessamzuniga-beep <vanessamzuniga@gmail.com>
2026-06-08 18:54:47 -0500

    lab

--- index.js
+++ index.js
@@ -3,11 +3,14 @@
 // Function to change the background color when a button is clicked
 function changeBackgroundColor() {
   // Implement the function to change background color
+  const changeColor = "#ebb8dd";
+  document.body.style.backgroundColor = changeColor;
 }
 
 // Function to reset the background color when the body is double-clicked
 function resetBackgroundColor() {
   // Implement the function to reset background color
+  document.body.style.backgroundColor = "";
 }
 
 // Capture Keyboard Input
@@ -15,14 +18,23 @@ function resetBackgroundColor() {
 // Function to display the key pressed by the user
 function displayKeyPress(event) {
   // Implement the function to display key pressed
+  const display = document.getElementById("keyPressDisplay");
+  display.textContent = "Key pressed: " + event.key;
 }
+
+document.addEventListener("keydown", displayKeyPress);
 
 // Process Text Input
 
 // Function to display user input in real-time
 function displayUserInput() {
   // Implement the function to display user input
+  const input = document.getElementById("textInput");
+  const output = document.getElementById("textInputDisplay")
+  output.textContent = "You typed: " + input.value;
 }
+
+document.addEventListener("input", displayUserInput)
 
 // Attach Event Listeners
 function setupEventListeners() {
